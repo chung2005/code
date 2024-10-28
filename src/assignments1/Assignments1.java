3e4742bbb98f401dab4b2215c3faaf5db80b6397
kieuducchung <kdc311005@gmail.com>
2024-10-28 12:44:13 +0700

    assignment1

---
+++ src/assignments1/Assignments1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package assignments1;
+
+
+public class Assignments1 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Kieu Duc Chung" );
+        System.out.println("MSSV: 29215153820" ); 
+        System.out.println("Lop: TPM15" );
+    }
+    
+}
